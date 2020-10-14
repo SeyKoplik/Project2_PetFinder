9995c49bf86a3f7e326e4605d89d024c53fec251
Lauren Brown <laurenbrown108@gmail.com>
2020-10-14 11:56:31 -0400

    all sensitive info should be protected with .env. You can make a .env folder on your end if you haven't already since it is part of the .gitignore.

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": process.env.DB_PASS,
     "database": "pets_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
