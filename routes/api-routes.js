e9e1087c188506435b05a024d8e179045c88a234
Lauren Brown <laurenbrown108@gmail.com>
2020-10-14 11:23:48 -0400

    revert

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,16 @@
 
+// var petfinder = require("@petfinder/petfinder-js");
+// var client = new petfinder.Client({ apiKey: "", secret: "" });
+// client.animal.search()
+// 	.then(function (response) {
+// 		// Do something with `response.data.animals`
+// 		//console.log(response.data);
+// 		console.log(response.data.animals)
+        
+// 	})
+// 	.catch(function (error) {
+// 		// Handle the error
+// 	});
 
 const db = require("../models");
 const passport = require("../config/passport");
@@ -52,28 +64,3 @@ module.exports = function(app) {
     }
   });
 };
-
-async function showAnimals(animalType, searchBreed) {
-  let page = 1;
-  do {
-    apiResult = await client.animal.search({
-      type: animalType,
-      breed: searchBreed,
-      page,
-      limit: 100,
-    });
-    let dogIdx = (page - 1) * 100;
-    apiResult.data.animals.forEach(function(animal) {
-      console.log(
-        ` -- ${++dogIdx}: ${animal.name} id: ${animal.id} age: ${animal.age}`
-      );
-    });
-    page++;
-  } while (
-    apiResult.data.pagination &&
-    apiResult.data.pagination.total_pages >= page
-  );
-}
-(async function() {
-  await showAnimals("Dog", "Alaskan Malamute");
-})();
