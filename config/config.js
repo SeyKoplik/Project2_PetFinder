c29a202079a7a70279974b81ff23220084a743e0
Lauren Brown <laurenbrown108@gmail.com>
2020-10-14 11:24:54 -0400

    modified config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.DB_PASS,
+    "password": "",
     "database": "pets_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
